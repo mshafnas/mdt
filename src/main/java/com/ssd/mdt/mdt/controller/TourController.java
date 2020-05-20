8dcbb877f4b0a9ff73b43490d0484f2371983160
Abdul Rahman <boomrahman12@gmail.com>
2020-05-20 14:52:20 +0530

    Update tour function

--- src/main/java/com/ssd/mdt/mdt/controller/TourController.java
+++ src/main/java/com/ssd/mdt/mdt/controller/TourController.java
@@ -3,13 +3,11 @@ package com.ssd.mdt.mdt.controller;
 import com.ssd.mdt.mdt.model.Tour;
 import com.ssd.mdt.mdt.repository.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -20,6 +18,7 @@ public class TourController {
 
     @Autowired
     private final TourRepository tourRepository;
+
     public TourController(TourRepository tourRepository) {
         this.tourRepository = tourRepository;
     }
@@ -32,7 +31,7 @@ public class TourController {
     }
 
     @GetMapping("list")
-    public String showUpdateForm(Model model) {
+    public String tours(Model model) {
         model.addAttribute("tours", this.tourRepository.findAll());
         return "tourList";
     }
@@ -45,4 +44,25 @@ public class TourController {
         this.tourRepository.save(tour);
         return "redirect:list";
     }
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm(@PathVariable ("id") long id,Model model){
+        Tour tour = this.tourRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Tour ID :" + id));
+
+        model.addAttribute("tour", tour);
+        return "updateTour";
+    }
+    @PostMapping("update/{id}")
+    public String updateTour(@PathVariable ("id") long id, @Valid Tour tour, BindingResult result, Model model){
+        if (result.hasErrors()){
+            tour.setId(id);
+            return "updateTour";
+        }
+        tourRepository.save(tour);
+
+        model.addAttribute("tours", this.tourRepository.findAll());
+        return "redirect:/tour/list";
+    }
+
 }
