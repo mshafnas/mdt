553c5734ec4e5ab5198129c0dbd7b9d9b8316eec
Abdul Rahman <boomrahman12@gmail.com>
2020-05-25 21:06:27 +0530

    Update Tour Booking

--- src/main/java/com/ssd/mdt/mdt/controller/TourBookingController.java
+++ src/main/java/com/ssd/mdt/mdt/controller/TourBookingController.java
@@ -1,9 +1,7 @@
 package com.ssd.mdt.mdt.controller;
 
-import com.ssd.mdt.mdt.model.Customer;
 import com.ssd.mdt.mdt.model.Tour;
 import com.ssd.mdt.mdt.model.TourBooking;
-import com.ssd.mdt.mdt.repository.CustomerRepository;
 import com.ssd.mdt.mdt.repository.TourBookingRepository;
 import com.ssd.mdt.mdt.repository.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,5 +49,25 @@ public class TourBookingController {
         }
         this.tourBookingRepository.save(tourBooking);
         return "redirect:list";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm(@PathVariable ("id") long id,Model model){
+        TourBooking tourBooking = this.tourBookingRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Tour Booking ID :" + id));
+
+        model.addAttribute("tourBooking", tourBooking);
+        return "updateTourBook";
+    }
+    @PostMapping("update/{id}")
+    public String updateTourBook(@PathVariable ("id") long id, @Valid TourBooking tourBooking, BindingResult result, Model model){
+        if (result.hasErrors()){
+            tourBooking.setId(id);
+            return "updateTourBook";
+        }
+        tourBookingRepository.save(tourBooking);
+
+        model.addAttribute("tourBookings", this.tourBookingRepository.findAll());
+        return "redirect:/booking/list";
     }
 }
